685a96461bd3c94282bdae083332b674a9f81f29
suntff <157761880+suntfff@users.noreply.github.com>
2024-03-02 11:51:54 +0200

    Create Rectangle.cpp

---
+++ src/Rectangle.cpp
@@ -0,0 +1,16 @@
+#include <Figures.hpp>
+namespace msp {
+	Rectangle::Rectangle(Point p1, Point p2) {
+		Setup(p1, p2);
+	}
+	void Rectangle::Setup(Point p1, Point p2) {
+		shape.setFillColor(sf::Color::Blue);
+		shape.setPosition(p1.x, p1.y);
+		shape.setSize(sf::Vector2f(p1.x, p1.y));
+		shape.setFillColor(sf::Color::Blue);
+		shape.setPosition(p2.x, p2.y);
+	}
+	sf::RectangleShape Rectangle::Get() {
+		return shape;
+	}
+}
