90b3c29d51dd74d1b61984ed375cf3f9c23e8071
suntff <157761880+suntfff@users.noreply.github.com>
2024-03-02 11:51:39 +0200

    Create Circle.cpp

---
+++ src/Circle.cpp
@@ -0,0 +1,14 @@
+#include <Figures.hpp>
+namespace msp{
+	Circle::Circle(Point p1, float r){
+		Setup(p1, r);
+	}
+	void Circle::Setup(Point p1, float r){
+		shape.setRadius(r);
+		shape.setPosition(p1.x, p1.y);
+		shape.setFillColor(sf::Color::Green);
+	}
+	sf::CircleShape Circle::Get(){
+		return shape;
+	}
+}
