ad01b5fb1d07db32548fdbcc4e5fa668bc65b0e7
suntff <157761880+suntfff@users.noreply.github.com>
2024-03-02 11:51:19 +0200

    Create Figures.hpp

---
+++ inc/Figures.hpp
@@ -0,0 +1,41 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+namespace msp {
+	struct Point {
+		int x;int y;
+	};
+	class Triangle {
+	   sf::ConvexShape shape;
+	public:
+		Triangle() = default;
+		Triangle(Point p1, Point p2, Point p3, Point p4);
+		void Setup(Point p1, Point p2, Point p3, Point p4);
+		sf::ConvexShape Get();
+	};
+	class Circle{
+	    sf::CircleShape shape;
+	public:
+		Circle() = default;
+		Circle(Point p1, float r);
+		void Setup(Point p1, float r);
+		sf::CircleShape Get();
+	};
+	class Rectangle {
+		sf::RectangleShape shape;
+	public:
+		Rectangle() = default;
+		Rectangle(Point p1, Point p2);
+		void Setup(Point p1, Point p2);
+		sf::RectangleShape Get();
+	};
+	class Line {
+		sf::VertexArray shape;
+
+	public:
+		Line() : shape(sf::Lines, 2) {}
+		Line(Point p1, Point p2);
+		void Setup(Point p1, Point p2);
+		sf::VertexArray Get();
+	};
+
+}
