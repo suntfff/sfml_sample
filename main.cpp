b502b035fdccf40f4109140a921d12b584623a87
suntff <157761880+suntfff@users.noreply.github.com>
2024-03-14 14:27:53 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 ﻿#include <SFML/Graphics.hpp>
-#include <Figures.hpp>
+#include <Circle.hpp>
 #include <Game.hpp>
 int main()
 {
     srand(time(0));
-    msp::Game game(2000, 1000, "Game!!!",6);
-    game.Setup_Circle();game.Setup_Rectangle(); game.Setup_Line(); game.Setup_Triangle();
+    msp::Game game(1800, 900, "Game!!!");
+    game.Setup_Circle();
     game.LifeCycle();
     return 0;
 }
